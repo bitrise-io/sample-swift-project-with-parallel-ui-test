1ba78749af96b8cee1fad6d9e359ac85af052d19
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2021-06-17 09:34:11 +0200

    Added ParallelUITests tes plan.

--- BullsEyeUITests/BullsEyeUITests.swift
+++ BullsEyeUITests/BullsEyeUITests.swift
@@ -63,3 +63,39 @@ class BullsEyeUITests: XCTestCase {
     }
   }
 }
+
+class BullsEyeUITests2: XCTestCase {
+  var app: XCUIApplication!
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    continueAfterFailure = false
+    app = XCUIApplication()
+    app.launch()
+  }
+  
+  func testGameStyleSwitch() {
+    // given
+    let slideButton = app.segmentedControls.buttons["Slide"]
+    let typeButton = app.segmentedControls.buttons["Type"]
+    let slideLabel = app.staticTexts["Get as close as you can to: "]
+    let typeLabel = app.staticTexts["Guess where the slider is: "]
+    
+    // then
+    if slideButton.isSelected {
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+
+      typeButton.tap()
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+    } else if typeButton.isSelected {
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+
+      slideButton.tap()
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+    }
+  }
+}
