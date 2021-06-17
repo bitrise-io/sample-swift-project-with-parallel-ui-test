ab3df996d6771034b5404055ff3a61e82f149459
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2021-06-17 10:06:17 +0200

    Add FailingTests test plan.

--- BullsEyeSlowTests/BullsEyeSlowTests.swift
+++ BullsEyeSlowTests/BullsEyeSlowTests.swift
@@ -99,3 +99,41 @@ class BullsEyeSlowTests: XCTestCase {
     XCTAssertEqual(statusCode, 200)
   }
 }
+
+class BullsEyeFailingTests: XCTestCase {
+  
+  var sut: URLSession!
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    sut = URLSession(configuration: .default)
+  }
+  
+  override func tearDownWithError() throws {
+    sut = nil
+    try super.tearDownWithError()
+  }
+  
+  
+  func testApiCallCompletes() throws {
+    // given
+    let urlString = "http://www.randomnumberapi.com/fail"
+    let url = URL(string: urlString)!
+    let promise = expectation(description: "Completion handler invoked")
+    var statusCode: Int?
+    var responseError: Error?
+
+    // when
+    let dataTask = sut.dataTask(with: url) { _, response, error in
+      statusCode = (response as? HTTPURLResponse)?.statusCode
+      responseError = error
+      promise.fulfill()
+    }
+    dataTask.resume()
+    wait(for: [promise], timeout: 5)
+
+    // then
+    XCTAssertNil(responseError)
+    XCTAssertEqual(statusCode, 200)
+  }
+}
