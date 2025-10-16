451662d224d4a315fe309f75f4da1fab1d80242c
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-10-16 13:12:22 +0200

    Add more ui tests

--- BullsEyeUITests/BullsEyeUITests.swift
+++ BullsEyeUITests/BullsEyeUITests.swift
@@ -103,3 +103,163 @@ class BullsEyeUITests2: XCTestCase {
     }
   }
 }
+
+class BullsEyeUITests3: XCTestCase {
+  var app: XCUIApplication!
+  
+  override class var runsForEachTargetApplicationUIConfiguration: Bool {
+      true
+  }
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    continueAfterFailure = false
+    app = XCUIApplication()
+    app.launch()
+  }
+  
+  func testGameStyleSwitch() {
+    // given
+    let slideButton = app.segmentedControls.buttons["Slide"]
+    let typeButton = app.segmentedControls.buttons["Type"]
+    let slideLabel = app.staticTexts["Get as close as you can to: "]
+    let typeLabel = app.staticTexts["Guess where the slider is: "]
+    
+    // then
+    if slideButton.isSelected {
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+
+      typeButton.tap()
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+    } else if typeButton.isSelected {
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+
+      slideButton.tap()
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+    }
+  }
+}
+
+class BullsEyeUITests4: XCTestCase {
+  var app: XCUIApplication!
+  
+  override class var runsForEachTargetApplicationUIConfiguration: Bool {
+      true
+  }
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    continueAfterFailure = false
+    app = XCUIApplication()
+    app.launch()
+  }
+  
+  func testGameStyleSwitch() {
+    // given
+    let slideButton = app.segmentedControls.buttons["Slide"]
+    let typeButton = app.segmentedControls.buttons["Type"]
+    let slideLabel = app.staticTexts["Get as close as you can to: "]
+    let typeLabel = app.staticTexts["Guess where the slider is: "]
+    
+    // then
+    if slideButton.isSelected {
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+
+      typeButton.tap()
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+    } else if typeButton.isSelected {
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+
+      slideButton.tap()
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+    }
+  }
+}
+
+class BullsEyeUITests5: XCTestCase {
+  var app: XCUIApplication!
+  
+  override class var runsForEachTargetApplicationUIConfiguration: Bool {
+      true
+  }
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    continueAfterFailure = false
+    app = XCUIApplication()
+    app.launch()
+  }
+  
+  func testGameStyleSwitch() {
+    // given
+    let slideButton = app.segmentedControls.buttons["Slide"]
+    let typeButton = app.segmentedControls.buttons["Type"]
+    let slideLabel = app.staticTexts["Get as close as you can to: "]
+    let typeLabel = app.staticTexts["Guess where the slider is: "]
+    
+    // then
+    if slideButton.isSelected {
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+
+      typeButton.tap()
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+    } else if typeButton.isSelected {
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+
+      slideButton.tap()
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+    }
+  }
+}
+
+class BullsEyeUITests6: XCTestCase {
+  var app: XCUIApplication!
+  
+  override class var runsForEachTargetApplicationUIConfiguration: Bool {
+      true
+  }
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    continueAfterFailure = false
+    app = XCUIApplication()
+    app.launch()
+  }
+  
+  func testGameStyleSwitch() {
+    // given
+    let slideButton = app.segmentedControls.buttons["Slide"]
+    let typeButton = app.segmentedControls.buttons["Type"]
+    let slideLabel = app.staticTexts["Get as close as you can to: "]
+    let typeLabel = app.staticTexts["Guess where the slider is: "]
+    
+    // then
+    if slideButton.isSelected {
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+
+      typeButton.tap()
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+    } else if typeButton.isSelected {
+      XCTAssertTrue(typeLabel.exists)
+      XCTAssertFalse(slideLabel.exists)
+
+      slideButton.tap()
+      XCTAssertTrue(slideLabel.exists)
+      XCTAssertFalse(typeLabel.exists)
+    }
+  }
+}
